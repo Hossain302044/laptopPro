0ac30125a4e72a454250275b0cd2a3dba25085ab
Hossain302044 <Hossain30204>
2022-05-01 01:41:18 +0600

    blog section design

--- src/component/Page/Blog/Blog.js
+++ src/component/Page/Blog/Blog.js
@@ -3,7 +3,30 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-
+            <section className="text-gray-600 body-font overflow-hidden">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="-my-8 divide-y-2 divide-gray-100">
+                        <div className="py-8 flex flex-wrap md:flex-nowrap">
+                            <div className="md:flex-grow">
+                                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
+                                <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+                            </div>
+                        </div>
+                        <div className="py-8 flex flex-wrap md:flex-nowrap">
+                            <div className="md:flex-grow">
+                                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Meditation bushwick direct trade taxidermy shaman</h2>
+                                <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+                            </div>
+                        </div>
+                        <div className="py-8 flex flex-wrap md:flex-nowrap">
+                            <div className="md:flex-grow">
+                                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
+                                <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
